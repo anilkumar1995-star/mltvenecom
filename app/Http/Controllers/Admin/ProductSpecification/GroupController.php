3777da61964e13fb9a13db5104f9c46af8e745d3
VickyRajput999 <154819926+VickyRajput999@users.noreply.github.com>
2026-02-04 10:24:37 +0530

    pull main

--- app/Http/Controllers/Admin/ProductSpecification/GroupController.php
+++ app/Http/Controllers/Admin/ProductSpecification/GroupController.php
@@ -18,8 +18,6 @@ class GroupController extends Controller
         $this->middleware('auth');
     }
 
-    // --- Specification Groups ---
-
     public function index(Request $request)
     {
         $query = EcSpecificationGroup::query();
@@ -228,7 +226,7 @@ class GroupController extends Controller
         $data['tables'] = EcSpecificationTable::orderBy('id', 'desc')->get();
         return view('admin-layouts.product-specification.tables.index', $data);
     }
-    
+
     public function productTablecreate()
     {
         $data['groups'] = EcSpecificationGroup::all();
